7539b43f736af954d7abdc94ec6f3633604e3479
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2022-12-28 12:21:04 +1030

    Tidy day 16

--- src/P16.hs
+++ src/P16.hs
@@ -1,16 +1,17 @@
 module P16 (run1, run2, inputLocation) where
 
+import qualified Data.Set as S
 import qualified Data.Map as M
 import Data.List (delete, (\\))
-import Data.Maybe (mapMaybe)
 
 -- pressure, neighbours
 data Valve = Valve Int [String]
-type Input = M.Map String Valve
+type ValveMap = M.Map String Valve
 
 -- name, pressure, distance to nodes
 type DistanceMap = M.Map String Int
 data Node = Node Int DistanceMap deriving (Eq, Ord, Show)
+data Input = Input DistanceMap [(String, Node)]
 type Network = M.Map String Node
 
 type UnvisitedNodes = M.Map String (Maybe Int)
@@ -25,15 +26,14 @@ run2 :: String -> Int
 run2 = solve2 . parse
 
 parse :: String -> Input
-parse = M.fromList . map parseLine . lines
+parse = nodePaths . M.fromList . map (parseLine . words) . lines
 
-parseLine :: String -> (String, Valve)
-parseLine s =
-    let ws = words s
-        name = ws !! 1
-        pressure = parsePressure (ws !! 4)
-        neighbours = map parseNeighbour (drop 9 ws)
+parseLine :: [String] -> (String, Valve)
+parseLine (_:name:_:_:pressureStr:_:_:_:_:neighboursStr) =
+    let pressure = parsePressure pressureStr
+        neighbours = map parseNeighbour neighboursStr
     in  (name, Valve pressure neighbours)
+parseLine e = error ("Error parsing input: " ++ unwords e)
 
 parseNeighbour :: String -> String
 parseNeighbour = take 2
@@ -42,25 +42,43 @@ parsePressure :: String -> Int
 parsePressure = read . drop 5 . init
 
 solve1 :: Input -> Int
-solve1 = findMaxPressure 30 . nodePaths
+solve1 = findMaxPressure 30
 
 solve2 :: Input -> Int
-solve2 = findMaxPressure2 26 . nodePaths
+solve2 = findMaxPressure2 26
 
-nodePaths :: Input -> (DistanceMap, [(String, Node)])
+nodePaths :: ValveMap -> Input
 nodePaths input =
-    let valves = M.filter (> 0) $ M.map (\(Valve pressure _) -> pressure) input
-        network = M.elems $ M.mapWithKey (node input (M.keys valves)) valves
-        initDistances = distances input (M.keys valves) "AA"
-    in  (initDistances, network)
+    let valves = nonZeroValvePressures input
+    in  Input (initDistances input valves) (mapNodes input valves)
 
-distances :: Input -> [String] -> String -> M.Map String Int
-distances input destinations start = M.fromList $ zip destinations $ mapMaybe (distance input start) destinations
+initDistances :: ValveMap -> M.Map String Int -> M.Map String Int
+initDistances input = distances input "AA" . M.keys
 
-distance :: Input -> String -> String -> Maybe Int
-distance input start = dijkstra input (M.fromList $ map (\k -> (k, Nothing)) $ M.keys input) (Just (start, 0))
+mapNodes :: ValveMap -> M.Map String Int -> [(String, Node)]
+mapNodes input nonZeroValves = map (node input valveNames) $ M.toList nonZeroValves
+    where valveNames = M.keys nonZeroValves
 
-dijkstra :: Input -> UnvisitedNodes -> Maybe (String, Int) -> String -> Maybe Int
+nonZeroValvePressures :: ValveMap -> M.Map String Int
+nonZeroValvePressures = M.filter (> 0) . M.map (\(Valve pressure _) -> pressure)
+
+distances :: ValveMap -> String -> [String] -> M.Map String Int
+distances input start = stripNothings . mapFromKeys (distance input start)
+
+mapFromKeys :: Ord a => (a -> b) -> [a] -> M.Map a b
+mapFromKeys f = M.fromSet f . S.fromList
+
+stripNothings :: M.Map a (Maybe b) -> M.Map a b
+stripNothings = M.mapMaybe id
+
+distance :: ValveMap -> String -> String -> Maybe Int
+distance input start = dijkstra input (initUnvisitedNodes input) initialNode
+    where initialNode = Just (start, 0)
+
+initUnvisitedNodes :: ValveMap -> UnvisitedNodes
+initUnvisitedNodes = M.map (const Nothing)
+
+dijkstra :: ValveMap -> UnvisitedNodes -> Maybe (String, Int) -> String -> Maybe Int
 dijkstra _ _ Nothing _ = Nothing
 dijkstra input unvisitedNodes (Just (currentNode, currentDistance)) targetNode
     | currentNode == targetNode = Just currentDistance
@@ -79,19 +97,14 @@ minTentativeDistance prev@(Just (_, prevDistance)) key (Just tentativeDistance)
     | tentativeDistance < prevDistance = Just (key, tentativeDistance)
     | otherwise = prev
 
-updateUnvisited :: Input -> Int -> String -> UnvisitedNodes -> UnvisitedNodes
+updateUnvisited :: ValveMap -> Int -> String -> UnvisitedNodes -> UnvisitedNodes
 updateUnvisited input nextDistance currentNode unvisitedNodes = M.delete currentNode $ updateNeighbours input nextDistance unvisitedNodes currentNode
 
-updateNeighbours :: Input -> Int -> UnvisitedNodes -> String -> UnvisitedNodes
+updateNeighbours :: ValveMap -> Int -> UnvisitedNodes -> String -> UnvisitedNodes
 updateNeighbours valves currentDistance unvisitedNodes = foldl (updateNeighbour currentDistance) unvisitedNodes . reachableNeighbours valves
 
-reachableNeighbours :: Input -> String -> [String]
-reachableNeighbours input node = case M.lookup node input of
-    Nothing -> error ("error looking up node " ++ node)
-    Just x -> neighbours x
-
-neighbours :: Valve -> [String]
-neighbours (Valve _ n) = n
+reachableNeighbours :: ValveMap -> String -> [String]
+reachableNeighbours input nodeName = case input M.! nodeName of (Valve _ neighbours) -> neighbours
 
 updateNeighbour :: Int -> UnvisitedNodes -> String -> UnvisitedNodes
 updateNeighbour currentDistance unvisitedNodes key =
@@ -101,19 +114,19 @@ updateDistance :: Int -> Maybe Int -> Maybe Int
 updateDistance d Nothing = Just d
 updateDistance dNew (Just dOld) = Just (min dNew dOld)
 
-node :: Input -> [String] -> String -> Int -> (String, Node)
-node input destinations start pressure = (start, Node pressure $ distances input (delete start destinations) start)
+node :: ValveMap -> [String] -> (String, Int) -> (String, Node)
+node input destinations (start, pressure) = (start, Node pressure $ distances input start (delete start destinations))
 
-findMaxPressure :: Int -> (DistanceMap, [(String, Node)]) -> Int
-findMaxPressure _ (_, []) = 0
-findMaxPressure timeLimit (initDistances, nodes) = totalPressure initDistances timeLimit (M.fromList nodes)
+findMaxPressure :: Int -> Input -> Int
+findMaxPressure _ (Input _ []) = 0
+findMaxPressure timeLimit (Input distanceMap nodes) = highestPossiblePressure distanceMap timeLimit (M.fromList nodes)
 
-findMaxPressure2 :: Int -> (DistanceMap, [(String, Node)]) -> Int
-findMaxPressure2 timeLimit (initDistances, network) = maximum $ map (maxPressureForPermutation timeLimit initDistances) $ permute network
+findMaxPressure2 :: Int -> Input -> Int
+findMaxPressure2 timeLimit (Input distanceMap network) = maximum $ map (maxPressureForPermutation timeLimit distanceMap) $ permute network
 
 maxPressureForPermutation :: Int -> DistanceMap -> ([(String, Node)], [(String, Node)]) -> Int
-maxPressureForPermutation timeLimit initDistances (valves1, valves2) =
-    totalPressure initDistances timeLimit (M.fromList valves1) + totalPressure initDistances timeLimit (M.fromList valves2)
+maxPressureForPermutation timeLimit distanceMap (valves1, valves2) =
+    highestPossiblePressure distanceMap timeLimit (M.fromList valves1) + highestPossiblePressure distanceMap timeLimit (M.fromList valves2)
 
 permute :: Ord a => [a] -> [([a], [a])]
 permute xs = removeDuplicateComplements $ map (pairWithComplement xs) $ partitions xs
@@ -136,16 +149,19 @@ addComplementPair acc (x,y)
     | M.member y acc = acc
     | otherwise = M.insert x y acc
 
-totalPressure :: M.Map String Int -> Int -> Network -> Int
-totalPressure nodeDistances remainingTime remainingNodes = safeMaximum (M.mapWithKey totalPressure' remainingNodes)
-    where   totalPressure' nextNodeName (Node nextNodePressure nextNodeDistances) = 
-                let remainingTime' = remainingTime - 1 - (nodeDistances M.! nextNodeName)
-                in  if remainingTime' <= 0
-                    then 0
-                    else
-                        let pressure = remainingTime' * nextNodePressure
-                            remainingNodes' = M.delete nextNodeName remainingNodes
-                        in  pressure + totalPressure nextNodeDistances remainingTime' remainingNodes'
+highestPossiblePressure :: M.Map String Int -> Int -> Network -> Int
+highestPossiblePressure nodeDistances remainingTime remainingNodes = safeMaximum (M.mapWithKey (moveToNode nodeDistances remainingTime remainingNodes) remainingNodes)
+
+moveToNode :: M.Map String Int -> Int -> Network -> String -> Node -> Int
+moveToNode nodeDistances remainingTime remainingNodes nextNodeName (Node nextNodePressure nextNodeDistances)
+    | remainingTime' <= 0 = 0
+    | otherwise = pressure + highestPossiblePressure nextNodeDistances remainingTime' remainingNodes'
+        where   remainingTime' = decreaseRemainingTime remainingTime nodeDistances nextNodeName
+                pressure = remainingTime' * nextNodePressure
+                remainingNodes' = M.delete nextNodeName remainingNodes
+
+decreaseRemainingTime :: Int -> M.Map String Int -> String -> Int
+decreaseRemainingTime remainingTime nodeDistances nextNodeName = remainingTime - 1 - (nodeDistances M.! nextNodeName)
 
 safeMaximum :: M.Map k Int -> Int
 safeMaximum = safe' . M.elems
