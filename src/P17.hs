7cff374390d1ec6b97726f0f94d70e06c333f7e5
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-01-06 15:11:52 +1030

    Tidy day 17

--- src/P17.hs
+++ src/P17.hs
@@ -10,8 +10,9 @@ type Coord = (Int, Int)
 type Rock = S.Set Coord
 
 -- top 10 row state, jet index, rock index
-type State2 = (Rock, Int, Int)
-type StateCache = M.Map State2 (Int, Int)
+type State = (Rock, Int, Int)
+-- Map state to (rockCount, height)
+type StateCache = M.Map State (Int, Int)
 
 inputLocation :: String
 inputLocation = "inputs/input17"
@@ -31,13 +32,19 @@ parseJetChar '>' = JetRight
 parseJetChar c = error ("bad jet char: " ++ [c])
 
 solve :: Int -> [JetDirection] -> Int
-solve rockCount jetDirections = iterate2 (length jetDirections) M.empty rockCount (S.fromList (map (`pair` 0) [0..6])) (setYCoord (head rocks) 4) 0 (tail rocks ++ cycle rocks) (cycle jetDirections) 0 1
+solve rockCount jetDirections = iterateRock jetCycleLength M.empty rockCount rockFloor initialRock 0 rockCycle jetCycle 0 1
+    where jetCycleLength = length jetDirections
+          rockFloor = S.fromList (map (`pair` 0) [0..6])
+          initialRock = setRockHeight 4 (head rocks)
+          rockCycle = tail rocks ++ cycle rocks
+          jetCycle = cycle jetDirections
 
 pair :: Int -> Int -> Coord
 pair x y= (x,y)
 
 rocks :: [Rock]
-rocks = [S.fromList [(2,0),(3,0),(4,0),(5,0)], S.fromList [(2,1), (3,0), (3,1), (3,2), (4,1)], S.fromList [(2,0),(3,0),(4,0),(4,1),(4,2)], S.fromList [(2,0),(2,1),(2,2),(2,3)], S.fromList [(2,0),(2,1),(3,0),(3,1)]]
+rocks = [S.fromList [(2,0),(3,0),(4,0),(5,0)], S.fromList [(2,1), (3,0), (3,1), (3,2), (4,1)], S.fromList [(2,0),(3,0),(4,0),(4,1),(4,2)],
+            S.fromList [(2,0),(2,1),(2,2),(2,3)], S.fromList [(2,0),(2,1),(3,0),(3,1)]]
 
 dropRock1 :: Rock -> Rock -> Rock
 dropRock1 = moveRock down
@@ -47,17 +54,20 @@ blowRock JetLeft = moveRock left
 blowRock JetRight = moveRock right
 
 moveRock :: (Coord -> Coord) -> Rock -> Rock -> Rock
-moveRock dir state rock =
-    let rock' = S.map dir rock
-    in  if inBounds rock' && S.disjoint state rock'
-        then rock'
-        else rock
+moveRock dir state rock
+    | collision state rock' = rock
+    | inBounds rock' = rock'
+    | otherwise = rock
+        where rock' = S.map dir rock
 
 inBounds :: Rock -> Bool
 inBounds = all inBounds'
 
 inBounds' :: Coord -> Bool
 inBounds' (x,_) = x >= 0 && x < 7
+
+collision :: Rock -> Rock -> Bool
+collision a b = not $ S.disjoint a b
 
 down :: Coord -> Coord
 down (x,y) = (x, y-1)
@@ -68,57 +78,62 @@ left (x,y) = (x-1, y)
 right :: Coord -> Coord
 right (x,y) = (x+1, y)
 
-setYCoord :: Rock -> Int -> Rock
-setYCoord rock y = S.map (setYCoord' y) rock
+setRockHeight :: Int -> Rock -> Rock
+setRockHeight y = S.map (setYCoord y)
 
-setYCoord' :: Int -> Coord -> Coord
-setYCoord' h (x,y) = (x, y+h)
+setYCoord :: Int -> Coord -> Coord
+setYCoord h (x,y) = (x, y+h)
 
-purgeLowRocks :: Int -> Rock -> Rock
-purgeLowRocks height = S.filter (overHeight (height - 50))
-
-overHeight :: Int -> Coord -> Bool
-overHeight h (_,y)= y > h
-
-iterate2 :: Int -> StateCache -> Int -> Rock -> Rock -> Int -> [Rock] -> [JetDirection] -> Int -> Int -> Int
-iterate2 _ _ _ _ _ _ _ [] _ _ = error "End of infinite list"
-iterate2 jetLength stateCache targetRock stoppedRocks currentRock height nextRocks (jet:jets) jetCount currentRockCount =
+iterateRock :: Int -> StateCache -> Int -> Rock -> Rock -> Int -> [Rock] -> [JetDirection] -> Int -> Int -> Int
+iterateRock _ _ _ _ _ _ _ [] _ _ = error "End of infinite jets list"
+iterateRock jetLength stateCache targetRockCount stoppedRocks currentRock height nextRocks (jet:jets) jetCount currentRockCount =
     let rock' = blowRock jet stoppedRocks currentRock
         rock'' = dropRock1 stoppedRocks rock'
-        stoppedRocks' = purgeLowRocks height $ S.union stoppedRocks rock''
-        height' = max height $ maximum $ S.toList (S.map snd rock'')
-        jetCount' = (jetCount + 1) `mod` jetLength
+        stoppedRocks' = S.union stoppedRocks rock''
+        height' = maxHeight height rock''
+        jetCount' = jetCount + 1
     in  if rock' == rock''
-        then stopRock jetLength stateCache targetRock stoppedRocks' height' nextRocks jets jetCount' currentRockCount
-        else iterate2 jetLength stateCache targetRock stoppedRocks rock'' height nextRocks jets jetCount' currentRockCount
+        then stopRock jetLength stateCache targetRockCount stoppedRocks' height' nextRocks jets jetCount' currentRockCount
+        else iterateRock jetLength stateCache targetRockCount stoppedRocks rock'' height nextRocks jets jetCount' currentRockCount
+
+maxHeight :: Int -> Rock -> Int
+maxHeight oldHeight = max oldHeight . maximum . S.toList . S.map snd
 
 stopRock :: Int -> StateCache -> Int -> Rock -> Int -> [Rock] -> [JetDirection] -> Int -> Int -> Int
 stopRock _ _ _ _ _ [] _ _ _ = error "End of infinite rocks list"
-stopRock jetLength stateCache targetRock stoppedRocks height (nextRock:rocks) jets jetCount currentRockCount
-    | currentRockCount == targetRock = height
+stopRock jetLength stateCache targetRockCount stoppedRocks height (nextRock:rockCycle) jets jetCount currentRockCount
+    | currentRockCount == targetRockCount = height
     | otherwise =
-        let state = (sampleRockTop height stoppedRocks, jetCount, currentRockCount `mod` 5)
+        let jetCount' = jetCount `mod` jetLength
+            state = (sampleRockTop height stoppedRocks, jetCount', currentRockCount `mod` 5)
             stateCache' = M.insert state (currentRockCount, height) stateCache
+            nextRockAtTop = setRockHeight (height + 4) nextRock
         in  case checkRepeatState state stateCache of
-                Just (cycleStart, heightStart) -> repeatState stateCache targetRock height cycleStart heightStart currentRockCount
-                Nothing -> iterate2 jetLength stateCache' targetRock stoppedRocks (setYCoord nextRock (height + 4)) height rocks jets jetCount (currentRockCount + 1)
+                Just (cycleStart, heightStart) -> repeatState stateCache targetRockCount height cycleStart heightStart currentRockCount
+                Nothing -> iterateRock jetLength stateCache' targetRockCount stoppedRocks nextRockAtTop height rockCycle jets jetCount' (currentRockCount + 1)
 
 repeatState :: StateCache -> Int -> Int -> Int -> Int -> Int -> Int
-repeatState cache targetRock height cycleStart startHeight cycleEnd =
-    let rockCountDelta = cycleEnd - cycleStart
-        iterations = (targetRock - cycleStart) `div` rockCountDelta
-        height' = iterations * (height - startHeight)
-        remainingCyclePoint = targetRock - iterations * rockCountDelta
-    -- in  error (show cycleStart ++ " " ++ show cycleEnd ++ " " ++ show rockCountDelta ++ " " ++ show iterations ++ " " ++ show height' ++ " " ++ show remainingCyclePoint
-    --             ++ " " ++ show startHeight ++ " " ++ show height)
-    in  case fmap snd $ find ((== remainingCyclePoint) . fst) $ M.elems cache of
-            Nothing -> error "can't look up cycle value"
-            Just additionalHeight -> height' + additionalHeight
+repeatState cache targetRockCount height cycleStart startHeight cycleEnd =
+    let cycleLength = cycleEnd - cycleStart
+        cycleIterations = (targetRockCount - cycleStart) `div` cycleLength
+        cycleHeight = height - startHeight
+        fastForwardHeight = cycleIterations * cycleHeight
+        rockCountRemainder = targetRockCount `mod` cycleLength
+    in  case lookupHeight rockCountRemainder cache of
+            Nothing -> error "rock count value not stored in cache"
+            Just additionalHeight -> fastForwardHeight + additionalHeight
 
-checkRepeatState :: State2 -> StateCache -> Maybe (Int, Int)
+lookupHeight :: Int -> StateCache -> Maybe Int
+lookupHeight rockCount = fmap snd . find ((== rockCount) . fst) . M.elems
+
+checkRepeatState :: State -> StateCache -> Maybe (Int, Int)
 checkRepeatState = M.lookup
 
 sampleRockTop :: Int -> Rock -> Rock
-sampleRockTop height rock = S.filter (\(_,y) -> y > 0) $ S.map (\(x,y) -> (x, y - height + 17)) rock
+sampleRockTop height = pruneRock . shiftToHeight height 17
 
--- 1592369438111
+shiftToHeight :: Int -> Int -> Rock -> Rock
+shiftToHeight oldHeight newHeight = S.map (\(x,y) -> (x, y - oldHeight + newHeight))
+
+pruneRock :: Rock -> Rock
+pruneRock = S.filter (\(_,y) -> y > 0)
