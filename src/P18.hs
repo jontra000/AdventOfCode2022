c25ab46f3275ba793d3d0b98e1790bab07479bb4
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-01-06 15:35:33 +1030

    Tidy day 18

--- src/P18.hs
+++ src/P18.hs
@@ -21,8 +21,7 @@ parse = S.fromList . map parseCoord . lines
 
 parseCoord :: String -> Coord
 parseCoord = parseCoord' . map read . splitOn ","
-    where parseCoord' :: [Int] -> Coord
-          parseCoord' (x:y:z:_) = (x,y,z)
+    where parseCoord' (x:y:z:_) = (x,y,z)
           parseCoord' xs = error ("Can't parse coords " ++ unwords (map show xs))
 
 solve :: State -> Int
@@ -36,19 +35,26 @@ neighbours (x,y,z) = [(x+1, y , z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y
 
 fillGaps :: State -> State
 fillGaps state =
+        let (maxX, maxY, maxZ) = maxValues state
+            isInBounds = (\(x,y,z) -> x >= 0 && x <= maxX && y >= 0 && y <= maxY && z >= 0 && z <= maxZ)
+            allCoords = [(x,y,z) | x <- [0..maxX], y <- [0..maxY], z <- [0..maxZ]]
+        in  foldl (floodFill isInBounds) state allCoords
+
+maxValues :: State -> (Int, Int, Int)
+maxValues state = 
     case (S.lookupMax $ S.map (\(x,_,_) -> x) state, S.lookupMax $ S.map (\(_,y,_) -> y) state, S.lookupMax $ S.map (\(_,_,z) -> z) state) of
-        (Just maxX, Just maxY, Just maxZ) ->
-            let inBounds = (\(x,y,z) -> x >= 0 && x <= maxX && y >= 0 && y <= maxY && z >= 0 && z <= maxZ)
-                allCoords = [(x,y,z) | x <- [0..maxX], y <- [0..maxY], z <- [0..maxZ]]
-            in foldl (fill' inBounds) state allCoords
-        _ -> error "Bad state"
+        (Just maxX, Just maxY, Just maxZ) -> (maxX, maxY, maxZ)
+        _ -> error "Empty state"
 
-fill' :: (Coord -> Bool) -> State -> Coord -> State
-fill' inBounds state c = fromMaybe state (fill inBounds state [c])
+floodFill :: (Coord -> Bool) -> State -> Coord -> State
+floodFill isInBounds state = fromMaybe state . floodFillCell isInBounds state . singleton
 
-fill :: (Coord -> Bool) -> State -> [Coord] -> Maybe State
-fill _ state [] = Just state
-fill inBounds' state (c:cs)
-    | S.member c state = fill inBounds' state cs
-    | inBounds' c = fill inBounds' (S.insert c state) (neighbours c ++ cs)
+floodFillCell :: (Coord -> Bool) -> State -> [Coord] -> Maybe State
+floodFillCell _ state [] = Just state
+floodFillCell isInBounds state (c:cs)
+    | S.member c state = floodFillCell isInBounds state cs
+    | isInBounds c = floodFillCell isInBounds (S.insert c state) (neighbours c ++ cs)
     | otherwise = Nothing
+
+singleton :: a -> [a]
+singleton = (: [])
